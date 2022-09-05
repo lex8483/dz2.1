f325d7f74c8f1f4951c6683b7b379619d4587b5c
lex8483 <lex8483@mail.ru>
2022-09-05 11:31:38 +0300

    dz2.2

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,2 +1,54 @@
-public class Hogwarts {
+public abstract class Hogwarts {
+    private String name;
+    private int powerMagic;
+    private int transgress;
+
+    public Hogwarts(String name, int powerMagic, int transgress) {
+        this.name = name;
+        this.powerMagic = powerMagic;
+        this.transgress = transgress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPowerMagic() {
+        return powerMagic;
+    }
+
+    public void setPowerMagic(int powerMagic) {
+        if (powerMagic < 0 || powerMagic > 100) {
+            throw new RuntimeException("Введено неверное значение");
+        }
+        this.powerMagic = powerMagic;
+    }
+
+    public int getTransgress() {
+        return transgress;
+    }
+
+    public void setTransgress(int transgress) {
+        if (transgress < 0 || transgress > 100) {
+            throw new RuntimeException("Введено неверное значение");
+        }
+        this.transgress = transgress;
+    }
+
+    public void compare(Hogwarts student) {
+        int sumStudent1 = this.powerMagic + this.transgress;
+        int sumStudent2 = student.powerMagic + student.transgress;
+        if (sumStudent1 > sumStudent2) {
+            System.out.println(this.name + " сильнее " + student.name);
+        } else if (sumStudent2 > sumStudent1) {
+            System.out.println(student.name + " сильнее " + this.name);
+        } else {
+            System.out.println(this.name + " равен по силе " + student.name);
+        }
+    }
+
 }
--- src/Kogtevran.java
+++ src/Kogtevran.java
@@ -1,2 +1,68 @@
-public class Kogtevran {
+public class Kogtevran extends Hogwarts {
+    private int wise;
+    private int witty;
+    private int creativity;
+
+    public Kogtevran(String name, int powerMagic, int transgress, int wise, int witty, int creativity) {
+        super(name, powerMagic, transgress);
+        this.wise = wise;
+        this.witty = witty;
+        this.creativity = creativity;
+    }
+
+    public int getWise() {
+        return wise;
+    }
+
+    public void setWise(int wise) {
+        if (wise < 0 || wise > 100) {
+            throw new RuntimeException("Введено неверное значение");
+        }
+        this.wise = wise;
+    }
+
+    public int getWitty() {
+        return witty;
+    }
+
+    public void setWitty(int witty) {
+        if (witty < 0 || witty > 100) {
+            throw new RuntimeException("Введено неверное значение");
+        }
+        this.witty = witty;
+    }
+
+    public int getCreativity() {
+        return creativity;
+    }
+
+    public void setCreativity(int creativity) {
+        if (creativity < 0 || creativity > 100) {
+            throw new RuntimeException("Введено неверное значение");
+        }
+        this.creativity = creativity;
+    }
+    public void compare(Kogtevran student) {
+        int sumStudent1 = this.wise + this.witty + this.creativity;
+        int sumStudent2 = student.wise + student.witty + student.creativity;
+        if (sumStudent1 > sumStudent2) {
+            System.out.println(this.getName() + " сильнее " + student.getName());
+        } else if (sumStudent2 > sumStudent1) {
+            System.out.println(student.getName() + " сильнее " + this.getName());
+        } else {
+            System.out.println(this.getName() + " равен по силе " + student.getName());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Kogtevran{" +
+                "name=" + getName() +
+                ", powerMagic=" + getPowerMagic() +
+                ", transgress=" + getTransgress() +
+                ", wise=" + wise +
+                ", witty=" + witty +
+                ", creativity=" + creativity +
+                '}';
+    }
 }
